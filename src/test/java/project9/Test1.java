ff84781b09d543beafe392e5d30dbf0d34ef2dfc
damodarareddy50 <niteesh50@gmail.com>
2022-04-09 11:14:40 +0530

    new project9 ..please review test cases

---
+++ src/test/java/project9/Test1.java
@@ -0,0 +1,14 @@
+package project9;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class Test1 {
+	@Test
+ public void Test2()
+ {
+		String actual="hai";
+		String expected="hai";
+	 Assert.assertEquals(actual, expected);
+ }
+}
